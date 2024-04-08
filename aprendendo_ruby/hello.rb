c097532f80bb0182713626418fb9cfd147ae8099
Maria Luiza Nery Pinto <marialuiza.nerypinto@yahoo.com.br>
2024-04-08 14:37:23 -0300

    nível2

--- aprendendo_ruby/hello.rb
+++ aprendendo_ruby/hello.rb
@@ -1,10 +1,24 @@
 nome = "Maria"
 idade = 18
+frase = "Olá! Meu nome é #{nome} e tenho #{idade} anos"
 
-puts "Olá! Meu nome é #{nome} e tenho #{idade} anos"
+puts frase
 puts "\nMeu nome ao contrário é #{nome.reverse} e tem #{nome.length} letras"
 
 puts "\nPosso multiplicar meu nome a quantidade de vezes da minha idade, veja: #{nome * idade}"
 
 puts "Agora quero inverter a minha idade, mas números não se invertem, então preciso transformá-lo em uma string usando '.to_s'. Veja: #{idade.to_s.reverse}"
 puts "\nEu também posso converter dados em número '.to_i' e em array 'to._a'"
+
+puts "Agora é com você!\nQual o seu nome?"
+nome2 = gets
+puts "Olá #{nome2}! Digite um número" 
+num1 = gets
+puts "Outro número"
+num2 = gets
+puts "E um último número"
+num3 = gets
+puts "O maior desses número é #{[num1, num2, num3].max}"
+puts "Agora vamos ordená-los para ficar mais bonito #{[num1, num2, num3].sort!}"
+
+puts " Tenho a frase '#{frase}', mas e seu eu trocar meu nome com o seu? '#{frase.gsub(nome, nome2)}'"
